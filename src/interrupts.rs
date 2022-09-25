163431bb5f53e25f55d895388bfad5bec8a246e3
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2022-09-25 10:30:27 +0200

    code organizing

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -13,16 +13,21 @@ pub static PICS: spin::Mutex<ChainedPics> =
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
+
+        // double fault
         unsafe {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
-
-        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
-        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        // breakpoint
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        // page fault
         idt.page_fault.set_handler_fn(page_fault_handler);
+        // timer
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        // keyboard
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
 
         idt
     };
