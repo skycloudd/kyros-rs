0cfa5158db3ece502c9f7b1be981d5cca0f168a8
Thomas Voss <mail@thomasvoss.com>
2022-09-30 23:12:37 +0200

    add a basic strings.h

---
+++ include/strings.h
@@ -0,0 +1,13 @@
+#ifndef __KYROS_STRINGS_H_
+#define __KYROS_STRINGS_H_
+
+#include <cdefs.h>
+#include <stddef.h>
+
+__DECLS_BEGIN
+
+void bzero(void *, size_t);
+
+__DECLS_END
+
+#endif /* !__KYROS_STRINGS_H_ */
---
+++ lib/libc/strings/bzero.c
@@ -0,0 +1,8 @@
+#include <string.h>
+#include <strings.h>
+
+void
+bzero(void *s, size_t n)
+{
+	(void) memset(s, 0, n);
+}
